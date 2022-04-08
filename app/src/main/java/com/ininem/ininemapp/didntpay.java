45115e8d18955d46f9753a82ab1f4c7a55a2e746
vacuna <ininemdigital@gmail.com>
2022-04-08 13:22:45 -0600

    e

--- app/src/main/java/com/ininem/ininemapp/didntpay.java
+++ app/src/main/java/com/ininem/ininemapp/didntpay.java
@@ -2,7 +2,12 @@ package com.ininem.ininemapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class didntpay extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class didntpay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_didntpay);
+        Button logout = findViewById(R.id.salir);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(),Login.class));
+                finish();
+
+            }
+        });
     }
+
 }
